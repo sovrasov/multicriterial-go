84f80a8c112063ede313cc633e7267e1d808c728
sovrasov <sovrasov.vlad@gmail.com>
2017-06-24 11:59:05 +0300

    Add implementation of the problem class

---
+++ src/mcoProblem.cpp
@@ -0,0 +1,51 @@
+#include "mcoProblem.hpp"
+
+void MCOProblem::AddCriterion(const functionType& criterion)
+{
+  mCriterions.push_back(criterion);
+}
+
+void MCOProblem::SetDomain(int dimension, const double* lb, const double* ub)
+{
+  mDimension = dimension;
+  mLeftBound.resize(mDimension);
+  mRightBound.resize(mDimension);
+
+  for(int i = 0; i < mDimension; i++)
+  {
+    mLeftBound[i] = lb[i];
+    mRightBound[i] = ub[i];
+  }
+}
+void MCOProblem::SetDomain(int dimension, std::initializer_list<double> lb,
+    std::initializer_list<double> ub)
+{
+  mDimension = dimension;
+  mLeftBound.reserve(mDimension);
+  mRightBound.reserve(mDimension);
+
+  for(auto it = lb.begin(); it != lb.end(); ++it)
+    mLeftBound.push_back(*it);
+
+  for(auto it = ub.begin(); it != ub.end(); ++it)
+    mRightBound.push_back(*it);
+}
+
+int MCOProblem::GetDimension() const
+{
+  return mDimension;
+}
+
+int MCOProblem::GetCriterionsNumber() const
+{
+  return mCriterions.size();
+}
+
+void MCOProblem::GetBounds(double* lb, double* ub) const
+{
+  for(int i = 0; i < mDimension; i++)
+  {
+    lb[i] = mLeftBound[i];
+    ub[i] = mRightBound[i];
+  }
+}
--- src/mcoProblem.hpp
+++ src/mcoProblem.hpp
@@ -7,18 +7,22 @@
 class MCOProblem
 {
 protected:
+  using functionType = std::function<double(const double*)>;
+
   int mDimension;
   int mCriterionsNumber;
+  std::vector<functionType> mCriterions;
   std::vector<double> mLeftBound;
   std::vector<double> mRightBound;
+
 public:
 
-  void AddCriterion(std::function<double(const double*)>);
+  void AddCriterion(const functionType& criterion);
   void SetDomain(int dimension, const double* lb, const double* ub);
   void SetDomain(int dimension, std::initializer_list<double> lb,
       std::initializer_list<double> ub);
 
-  int GetDimansion() const;
+  int GetDimension() const;
   int GetCriterionsNumber() const;
   void GetBounds(double* lb, double* ub) const;
 };
