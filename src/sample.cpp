7210421dd6a2a05f1ba38d8386befa8d5dffa1fe
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2017-06-29 10:24:16 +0300

    Add a command line parser

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,8 +1,20 @@
 #include "multicriterialSolver.hpp"
 #include <iostream>
+#include <cmdline.h>
 
 int main(int argc, const char** argv)
 {
+  cmdline::parser parser;
+  parser.add<int>("threadsNum", 'p', "number of threads in parallel method", false, 1,
+     cmdline::range(1, 32));
+  parser.add<int>("evolventTightness", 't', "", false, 12,
+        cmdline::range(9, 16));
+  parser.add<double>("reliability", 'r', "reliability parameter for the method",
+    false, 4.5, cmdline::range(1., 1000.));
+  parser.add<double>("accuracy", 'e', "accuracy of the method", false, 0.01);
+  parser.add<int>("itersLimit", 'l', "limit of iterations for the method", false, 2000);
+  parser.parse_check(argc, argv);
+
   MCOProblem problem;
   problem.AddCriterion(
     [](const double* y) -> double { return y[0]*y[0];  }
@@ -13,7 +25,10 @@ int main(int argc, const char** argv)
   problem.SetDomain(1, {-10.}, {10.});
 
   MCOSolver solver;
-  solver.SetParameters(SolverParameters(0.01, 4, 1, 2000));
+  solver.SetParameters(SolverParameters(parser.get<double>("accuracy"),
+    parser.get<double>("reliability"),
+    parser.get<int>("threadsNum"),
+    parser.get<int>("itersLimit")));
   solver.SetProblem(problem);
   solver.Solve();
   auto solution = solver.GetWeakOptimalPoints();
