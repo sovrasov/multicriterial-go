485a575b345b2669b580a2b8e17129ae87ee556f
sovrasov <sovrasov.vlad@gmail.com>
2017-06-30 19:40:12 +0300

    Update drawing script

--- scripts/draw_2d.py
+++ scripts/draw_2d.py
@@ -4,6 +4,9 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use('classic')
 
 def readPoints(fileName):
     lines = [line.rstrip('\n') for line in open(fileName)]
@@ -33,14 +36,25 @@ def main():
 
     Y, W = readPoints(sys.argv[1])
 
-    plt.xlabel(r'$f_1$')
-    plt.ylabel(r'$f_2$')
+    if len(W[0]) == 2:
+        plt.xlabel(r'$f_1$')
+        plt.ylabel(r'$f_2$')
 
-    plt.plot(W[:,0], W[:,1], 'ro')
+        plt.plot(W[:,0], W[:,1], 'ro')
 
-    plt.grid()
-    plt.savefig(sys.argv[2] + '.png', format = 'png', dpi = 200)
-    plt.clf()
+        plt.grid()
+        plt.savefig(sys.argv[2] + '.png', format = 'png', dpi = 200)
+        plt.clf()
+
+    if len(Y[0]) == 2:
+        plt.xlabel(r'$y_1$')
+        plt.ylabel(r'$y_2$')
+
+        plt.plot(Y[:,0], Y[:,1], 'go')
+
+        plt.grid()
+        plt.savefig(sys.argv[2] + '_points.png', format = 'png', dpi = 200)
+        plt.clf()
 
 if __name__ == '__main__':
     main()
