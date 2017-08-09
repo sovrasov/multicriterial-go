4e776a4ac6e58b561ac1819c7a95b01000b8da2f
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2017-08-09 15:39:10 +0300

    Cleanup in tests

--- test/solver_tests.cpp
+++ test/solver_tests.cpp
@@ -13,7 +13,7 @@ auto solution = solver.GetWeakOptimalPoints();
 
 TEST(Solver, unconstrained_run_smoke)
 {
-	auto parameters = SolverParameters(0.01, 0,	4, 1,	2000,	0); \
+	auto parameters = SolverParameters(0.01, 0,	4, 1,	2000,	0);
 	RUN_SOLVER(strongin);
 
 	ASSERT_LE(solver.GetIterationsNumber(), 2000);
@@ -22,7 +22,7 @@ TEST(Solver, unconstrained_run_smoke)
 
 TEST(Solver, unconstrained_parallel_run_smoke)
 {
-	auto parameters = SolverParameters(0.01, 0,	4, 4,	2000,	0); \
+	auto parameters = SolverParameters(0.01, 0,	4, 4,	2000,	0);
 	RUN_SOLVER(strongin);
 
 	ASSERT_LE(solver.GetIterationsNumber(), 500);
@@ -31,7 +31,7 @@ TEST(Solver, unconstrained_parallel_run_smoke)
 
 TEST(Solver, constrained_run_smoke)
 {
-	auto parameters = SolverParameters(0.01, 0,	4, 1,	2000,	0); \
+	auto parameters = SolverParameters(0.01, 0,	4, 1,	2000,	0);
 	RUN_SOLVER(chakong);
 
 	ASSERT_LE(solver.GetIterationsNumber(), 1000);
@@ -40,7 +40,7 @@ TEST(Solver, constrained_run_smoke)
 
 TEST(Solver, constrained_parallel_run_smoke)
 {
-	auto parameters = SolverParameters(0.01, 0,	4, 4,	2000,	0); \
+	auto parameters = SolverParameters(0.01, 0,	4, 4,	2000,	0);
 	RUN_SOLVER(chakong);
 
 	ASSERT_LE(solver.GetIterationsNumber(), 250);
